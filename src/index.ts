f2d41b68bf09b28086f880313b4f0200b930b722
Rosaline Karr <rose@rosalinekarr.com>
2018-05-28 12:42:36 -0400

    Added code to insert a canvas on the page and render to it
    I updated the index.ts and index.css files to inject an HTML5 canvas on the page and draw a black rectangle to it.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
 // Require Stylesheets
 import './index.css';
+
+// Add a canvas element to the DOM
+const canvasElement = document.createElement('canvas');
+const canvasContext = canvasElement.getContext("2d");
+document.body.appendChild(canvasElement);
+
+// Define a render function for the canvas
+const render = () => {
+  // Fetch the current pixel size of the canvas
+  const width = canvasElement.clientWidth;
+  const height = canvasElement.clientHeight;
+
+  // Draw a black rectangle over the entire canvas
+  canvasContext.beginPath();
+  canvasContext.rect(0, 0, width, height);
+  canvasContext.fillStyle = 'black';
+  canvasContext.fill();
+};
+
+// Attach event listeners for the render function
+document.addEventListener('DOMContentLoaded', render);
+window.addEventListener('resize', render);
